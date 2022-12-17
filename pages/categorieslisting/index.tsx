6b486409162461987d4e313c4e691eb941b27ff0
DespoinaNtakou <dnta@qnr.com.gr>
2022-12-17 19:03:13 +0200

    CHANGES

--- pages/categorieslisting/index.tsx
+++ pages/categorieslisting/index.tsx
@@ -1,18 +1,34 @@
-import React from "react";
 import books from "../json/books.json";
+import React, { useEffect, useState } from "react";
 
-export default function  CategoriesListing() {
+export default function CategoriesListing() {
+  const [bookCategories, setBookCategories] = useState([]);
 
-function categoriesListing(){
-   books.forEach((book) => {
-    console.log(book);
-   });
-};
+  //making a function to separate each object of the array books using forEach() array method
 
-categoriesListing();
+  let categories: any = [];
+  function categoriesListing() {
+    books.forEach((book) => {
+      console.log(book);
+      categories += book.categories;
+    });
+    console.log(categories);
+    const newCategories = categories.split(",");
+    console.log(newCategories);
+    setBookCategories(newCategories);
+    bookCategories.map((bookCategory, index) => {
+      (<div key={index}>{bookCategory}</div>)
+  });
+  }
+  
+  useEffect(() => {
+    categoriesListing();
+  },[]);
 
-
-    return( <>
-
-    </>);
+  return (
+    <>
+      <h1>Book Categories Page</h1>
+      {bookCategories}
+    </>
+  );
 }
