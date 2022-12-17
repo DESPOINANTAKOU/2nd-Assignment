89e85da4c189d0e1fef2792b9496a2e343126a0b
DespoinaNtakou <dnta@qnr.com.gr>
2022-12-17 19:49:15 +0200

    creation of the categoriesListing function

--- pages/categorieslisting/index.tsx
+++ pages/categorieslisting/index.tsx
@@ -1,12 +1,16 @@
 import books from "../json/books.json";
 import React, { useEffect, useState } from "react";
+import categoriesCss from '../../styles/categorieslisting.module.css';
 
 export default function CategoriesListing() {
   const [bookCategories, setBookCategories] = useState([]);
 
-  //making a function to separate each object of the array books using forEach() array method
-
   let categories: any = [];
+  //in this function we loop the json data through forEach() method and in each loop we add the categories of each object of
+  //the books json data to the categories variable. Then we have a string that we split it at the (,)  and  convert it to an array
+  //through the split method. The array is called newCategories and we store it to the state.Last but not least we map it so
+  //that we can render it through JSX in the UI.
+  useEffect(() => {
   function categoriesListing() {
     books.forEach((book) => {
       console.log(book);
@@ -17,18 +21,17 @@ export default function CategoriesListing() {
     console.log(newCategories);
     setBookCategories(newCategories);
     bookCategories.map((bookCategory, index) => {
-      (<div key={index}>{bookCategory}</div>)
-  });
+      <div className={categoriesCss.bookDivs} key={index}>{bookCategory}</div>;
+    });
   }
-  
-  useEffect(() => {
-    categoriesListing();
-  },[]);
+  categoriesListing();
+}, []);
+
 
   return (
     <>
       <h1>Book Categories Page</h1>
-      {bookCategories}
+      <div className={categoriesCss.parentOfBookDivs}>{bookCategories}</div>
     </>
   );
 }
