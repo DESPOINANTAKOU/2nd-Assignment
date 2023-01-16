ef4c9c7e75e4589186a95bf901966c371dd04707
DespoinaNtakou <dnta@qnr.com.gr>
2023-01-16 17:26:50 +0200

    deletion of cookies - not necessary here!

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,9 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { cookies } from 'next/headers';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse ) {
   
-  if (req.method == "POST") {
+  if (req.method === "POST") {
     const { username, password } = req.body;
     if (username === "admin" && password === "123") {
       res.status(200).json(true);
@@ -11,9 +10,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse ) {
       res.status(403).json(false);
     }
   }
-  else if(req.method == "GET"){
-    const cook = cookies();
-    console.log(cook);
-    res.status(200).json(true)
+  else if(req.method === "GET") {
+    res.status(200).json(true);
   }
 }
